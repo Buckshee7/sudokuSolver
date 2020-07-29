10bd13eeb8d1f1e94647d70ce9c73b71079781f4
Timothy Saudners <timsaunders88@gmail.com>
2020-07-29 19:19:47 +0100

    added solve function and test

--- src/sudoku.py
+++ src/sudoku.py
@@ -1,15 +1,12 @@
 class Sudoku:
     def __init__(self, grid):
         self.matrix = grid
-
-    def solve(self):
-        pass
     
     def find_next_none(self):
-        for row in range(len(self.matrix)):
-            for cell in range(len(self.matrix[row])):
-                if self.matrix[row][cell] == None:
-                    return (row, cell)
+        for row_index in range(len(self.matrix)):
+            for column_index in range(len(self.matrix[row_index])):
+                if self.matrix[row_index][column_index] == None:
+                    return (row_index, column_index)
 
     def check_valid(self, number, row_index, column_index):
         row_invalid = self.check_in_row(number, row_index)
@@ -46,6 +43,27 @@ class Sudoku:
             if number in row:
                 return True
         return False
+
+    def solve(self):
+        #finds next cell to target, returns if no cell
+        next_none = self.find_next_none()
+        if next_none == None:
+            print(self.matrix)
+            return self.matrix
+        #tries each number 1 to 9 and if valid sets cell to number
+        for number in range(1,10):
+            if self.check_valid(number, next_none[0], next_none[1]) == True:
+                self.matrix[next_none[0]][next_none[1]] = number
+                #recusring will check the next empty cell
+                #if the-rest-of-the-cells are ok return the current matrix
+                if self.solve() == self.matrix:
+                    return self.matrix
+                #otherwise, empty the cell of the number we just tried
+                self.matrix[next_none[0]][next_none[1]] = None
+        #if each number tried and none of them work - returns false to parent function call
+        return False
+
+
 
 
 
--- tests/test_sudoku.py
+++ tests/test_sudoku.py
@@ -67,3 +67,15 @@ class TestSudoku(unittest.TestCase):
     def test_check_valid_false(self):
         self.assertEqual(False, self.doku.check_valid(8, 0, 0))
     
+    def test_solve(self):
+        solution = [
+            [4, 3, 5, 2, 6, 9, 7, 8, 1],
+            [6, 8, 2, 5, 7, 1, 4, 9, 3],
+            [1, 9, 7, 8, 3, 4, 5, 6, 2],
+            [8, 2, 6, 1, 9, 5, 3, 4, 7],
+            [3, 7, 4, 6, 8, 2, 9, 1, 5],
+            [9, 5, 1, 7, 4, 3, 6, 2, 8],
+            [5, 1, 9, 3, 2, 6, 8, 7, 4],
+            [2, 4, 8, 9, 5, 7, 1, 3, 6],
+            [7, 6, 3, 4, 1, 8, 2, 5, 9]]
+        self.assertEqual(solution, self.doku.solve())
